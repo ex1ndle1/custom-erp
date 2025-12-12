1237a6c06d71e1163bfe2ec82474e78b84d9251b
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2025-12-12 21:20:24 +0500

    .

--- erp/admin.py
+++ erp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Course, Subject,Teacher
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Subject)
+admin.site.register(Teacher)
--- erp/views.py
+++ erp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import Course
 # Create your views here.
 
 
 def index(request):
-    return render(request , 'erp/index.html')
+    course = Course.objects.all()
+    return render(request , 'erp/index.html', {'courses':course})
