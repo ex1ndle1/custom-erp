fca003e6a5c3e23491d7a509546cc3aa9d0a616c
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2025-12-28 01:43:53 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -154,8 +154,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL = f'https://bfknjpdjincygyjexjog.supabase.co/storage/v1/object/public/media/'
+MEDIA_URL = f'{SUPABASE_URL}/storage/v1/object/public/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
