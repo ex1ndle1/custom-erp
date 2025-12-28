d875108a63bb212011a79f52537fedf91b9f04b2
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2025-12-29 00:49:38 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -89,26 +89,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-SUPABASE_URL = os.getenv('SUPABASE_URL')
-SUPABASE_SERVICE_KEY = os.getenv('SUPABASE_API_KEY')
+
 SUPABASE_BUCKET_NAME = 'media' 
 
+SUPABASE_PROJECT_ID = os.getenv("SUPABASE_PROJECT_ID")
+SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = SUPABASE_SERVICE_KEY
 AWS_SECRET_ACCESS_KEY = SUPABASE_SERVICE_KEY
 
-AWS_STORAGE_BUCKET_NAME = 'media'
-AWS_S3_ENDPOINT_URL = f'https://{SUPABASE_URL}.supabase.co/storage/v1/s3'
+AWS_STORAGE_BUCKET_NAME = "media"
+AWS_S3_ENDPOINT_URL = f"https://{SUPABASE_PROJECT_ID}.supabase.co/storage/v1/s3"
 
-AWS_S3_REGION_NAME = 'us-east-1'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_S3_ADDRESSING_STYLE = 'path'
+AWS_S3_REGION_NAME = "us-east-1"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_ADDRESSING_STYLE = "path"
 AWS_DEFAULT_ACL = None
 AWS_S3_FILE_OVERWRITE = False
 
-MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/'
-
+MEDIA_URL = f"https://{SUPABASE_PROJECT_ID}.supabase.co/storage/v1/object/public/{AWS_STORAGE_BUCKET_NAME}/"
 print("SUPABASE_PROJECT_ID:", bool(os.getenv("SUPABASE_URL")))
 print("ALL ENV KEYS:", list(os.environ.keys()))
 # Database
@@ -198,7 +199,3 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-
-
