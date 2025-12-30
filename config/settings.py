6664c4c7305fb04df953267a2caae205295db776
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2025-12-31 00:12:03 +0500

    ,.

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,8 @@ DATABASES = {
         
 
         default=os.getenv('DATABASE_URL'),
-        conn_max_age=600
+        conn_max_age=600,
+        conn_health_checks=True
     )
 }
 
@@ -180,7 +181,7 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-
+EMAIL_TIMEOUT = 10
 SITE_ID = 1
 
 SOCIALACCOUNT_PROVIDERS = {
